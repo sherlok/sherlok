e6e3b8913256f9e7b1c73d5f9f089cb98421d53c
renaud <renaud@apache.org>
2014-11-13 23:16:20 +0100

    updated editor

--- public/_editor/scripts/app.js
+++ public/_editor/scripts/app.js
@@ -1,61 +1,69 @@
-
 var app = angular.module('sherlok_editor', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
     'ngRoute'
 ]);
-
 app.config(function ($routeProvider) {
     $routeProvider.when('/', {
-        templateUrl: 'views/pipelines/list.html',
-        controller: 'pipelines-list'
-    }).when('/pipelines/create', {
-        templateUrl: 'views/pipelines/create.html',
-        controller: 'pipelines-create'
-     }).when('/pipelines/show', {
-        templateUrl: 'views/pipelines/show.html',
-        controller: 'pipelines-show'
+        templateUrl: 'views/pipelines.html',
+        controller: 'pipelines'
+    // }).when('/pipelines/create', {
+    //     templateUrl: 'views/pipelines/create.html',
+    //     controller: 'pipelines-create'
     }).otherwise({
         redirectTo: '/'
     })
 });
 
-app.controller('pipelines-list', function ($scope, $http) {
+// app.controller('pipelines', function ($scope, $http, $location) {
+//     $http.get('/pipelines').success(function (data) {
+//         $scope.pipelines = data;
+//     }).error(function (data, status) {
+//         console.log('Error ' + data)
+//     })
+
+//     $scope.showPipeline = function () {
+//         console.log('showwww');
+//     }
+//     $scope.createPipeline = function () {
+//         console.log($scope.pipeline);
+//         $http.put('/pipelines', $scope.pipeline).success(function (data) {
+//             $location.path('/');
+//         }).error(function (data, status) {
+//             console.log('Error ' + data)
+//         })
+//     }
+// });
+
+app.controller('pipelines', function PipelineController($scope, $http, $location) {
     $http.get('/pipelines').success(function (data) {
         $scope.pipelines = data;
     }).error(function (data, status) {
         console.log('Error ' + data)
     })
-    $scope.showPipeline = function () {
-        console.log('showwww');
-    }
-});
 
-app.controller('pipelines-show', function ($scope, $http, $location) {
-    $scope.pipeline = {
-        done: false
+    $scope.open = function(item){
+        if ($scope.isOpen(item)){
+            $scope.activepipe = undefined;
+        } else {
+            $scope.activepipe = item;
+        }
     };
-    $scope.createPipeline = function () {
-        console.log($scope.pipeline);
-        $http.put('/pipelines', $scope.pipeline).success(function (data) {
-            $location.path('/');
-        }).error(function (data, status) {
-            console.log('Error ' + data)
-        })
-    }
-});
 
-app.controller('pipelines-create', function ($scope, $http, $location) {
-    $scope.pipeline = {
-        done: false
+    $scope.isOpen = function(item){
+        return $scope.activepipe === item;
     };
-    $scope.createPipeline = function () {
-        console.log($scope.pipeline);
-        $http.put('/pipelines', $scope.pipeline).success(function (data) {
-            $location.path('/');
-        }).error(function (data, status) {
-            console.log('Error ' + data)
-        })
-    }
+
+    $scope.hasActivePipeOpen = function() {
+        return $scope.activepipe !== undefined;
+    };
+
+    $scope.close = function() {
+        $scope.activepipe = undefined;
+    };
+
+    $scope.beep = function() {
+        alert('beep from '+$scope.activepipe.name);
+    };
 });
