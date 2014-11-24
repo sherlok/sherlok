22281091ce714b12ab632dc0379b01dbc59b59e0
renaud <renaud@apache.org>
2014-11-24 10:57:38 +0100

    fixing openshift

--- src/main/java/org/sherlok/aether/AetherResolver.java
+++ src/main/java/org/sherlok/aether/AetherResolver.java
@@ -16,6 +16,7 @@
 package org.sherlok.aether;
 
 import static org.sherlok.utils.Create.list;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -35,21 +36,18 @@ import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
+import org.slf4j.Logger;
 
 /**
  * A helper to boot the repository system and a repository system session.
  */
 public class AetherResolver {
+    private static final Logger LOG = getLogger(AetherResolver.class);
 
+    /** Where to store the local artifacts */
     public static final String LOCAL_REPO_PATH = "local_repo";
 
     public static RepositorySystem newRepositorySystem() {
-        /*
-         * Aether's components implement org.eclipse.aether.spi.locator.Service
-         * to ease manual wiring and using the prepopulated
-         * DefaultServiceLocator, we only need to register the repository
-         * connector and transporter factories.
-         */
         DefaultServiceLocator locator = MavenRepositorySystemUtils
                 .newServiceLocator();
         locator.addService(RepositoryConnectorFactory.class,
@@ -63,7 +61,7 @@ public class AetherResolver {
             @Override
             public void serviceCreationFailed(Class<?> type, Class<?> impl,
                     Throwable exception) {
-                exception.printStackTrace();
+                LOG.error("Aether serviceCreation failed", exception);
             }
         });
 
@@ -81,10 +79,8 @@ public class AetherResolver {
 
         // session.setTransferListener(new ConsoleTransferListener());
         // session.setRepositoryListener(new ConsoleRepositoryListener());
-
         // uncomment to generate dirty trees
         // session.setDependencyGraphTransformer( null );
-
         return session;
     }
 
@@ -100,6 +96,10 @@ public class AetherResolver {
             repos.add(new RemoteRepository.Builder("local_default", "default",
                     localRepo.toURI().toURL().toString()).build());
         }
+        // Sherlok repo
+        repos.add(new RemoteRepository.Builder("sherlok_deps", "default",
+                "https://raw.githubusercontent.com/renaud/sherlok_mavenrepo/master/")
+                .build());
         // Maven central
         repos.add(new RemoteRepository.Builder("central", "default",
                 "http://central.maven.org/maven2/").build());
@@ -110,5 +110,4 @@ public class AetherResolver {
         }
         return repos;
     }
-
 }
