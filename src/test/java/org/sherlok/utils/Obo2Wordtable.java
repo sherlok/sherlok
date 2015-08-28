eb83313462276fcdd239852c79587ca502ca2780
renaud <renaud@apache.org>
2015-08-28 16:46:41 +0200

    more tests

--- src/test/java/org/sherlok/utils/Obo2Wordtable.java
+++ src/test/java/org/sherlok/utils/Obo2Wordtable.java
@@ -15,12 +15,15 @@
  */
 package org.sherlok.utils;
 
+import static org.apache.commons.io.FilenameUtils.removeExtension;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.uima.ruta.tag.obo.OBOOntology;
 import org.apache.uima.ruta.tag.obo.OboFormatException;
 import org.apache.uima.ruta.tag.obo.OntologyTerm;
@@ -38,26 +41,32 @@ public class Obo2Wordtable {
 
     private static final String DIR = "/Users/richarde/git2/neuroNER/resources/bluima/neuroner";
 
-    public static void main(String[] args) throws IOException, OboFormatException {
+    public static void main(String[] args) throws IOException,
+            OboFormatException {
 
+        // OBO
         for (String oboFile : new String[] {//
-        "GeneNames",//
-                "hbp_developmental_ontology",//
-                "hbp_layer_ontology", //
-                "hbp_morphology_ontology", "hbp_neurotransmitter_ontology" }) {
+        "GeneNames.obo",//
+                "hbp_brainregions_aba-syn.obo",//
+                "hbp_developmental_ontology.obo",//
+
+                "hbp_morphology_ontology.obo",//
+                "hbp_neurotransmitter_ontology.obo",//
+                "regions.obo" }) {
 
             OBOOntology obo = new OBOOntology();
-            obo.read(new File(DIR, oboFile + ".obo"));
+            obo.read(new File(DIR, oboFile));
 
             process(obo, oboFile);
         }
 
-        for (String roboFile : new String[] {//
-        "hbp_electrophysiology-triggers_ontology",
-                "hbp_electrophysiology_ontology" }) {
-            OBOOntology obo = RoboExpander.expand(new File(DIR, roboFile
-                    + ".robo"));
-            process(obo, roboFile);
+        // ROBO
+        for (String roboFile : new String[] {
+                "hbp_electrophysiology-triggers_ontology.robo",//
+                "hbp_electrophysiology_ontology.robo",//
+                "hbp_layer_ontology.robo" }) {
+            OBOOntology obo = RoboExpander.expand(new File(DIR, roboFile));
+            process(obo, removeExtension(roboFile));
         }
     }
 
@@ -65,8 +74,8 @@ public class Obo2Wordtable {
     private static void process(OBOOntology obo, String oboFile)
             throws IOException {
 
-        FileWriter wordtableWriter = new FileWriter(new File(DIR, oboFile
-                + ".csv"));
+        FileWriter wordtableWriter = new FileWriter(new File(DIR,
+                removeExtension(oboFile) + ".csv"));
 
         // TextFileWriter wordtableWriter = new TextFileWriter(new File(
         // WORDTABLE_OUTPUT, oboFile + ".csv"));
